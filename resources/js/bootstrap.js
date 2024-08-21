cf0cce98807606a57ec72e111d155bdcecb936c3
Hadil Charafeddine <hadil@Hadils-MacBook-Pro.local>
2024-08-21 12:21:58 -0400

    Added xsrftoken to vue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,7 +3,7 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true
-window.axios.defaults.withXSRFToken = true
+// window.axios.defaults.withXSRFToken = true
 window.axios.interceptors.response.use(
     response => response,
     error => {
